25d004f78890555e970c74aaa5c056ccccec776b
Alex Sheriff <asheriff97@gmail.com>
2024-04-10 23:25:25 -0400

    Added Doubling Down, Added Adjusted Score of an Ace to 1 or 11, Minor Bug Fixes

--- Miscellaneous/blackjack/BJ-TODO.md
+++ Miscellaneous/blackjack/BJ-TODO.md
@@ -6,16 +6,21 @@ TO-DO
 
 MAJOR
  - Add some form of graphic interface
+ - Allow option to make a Ace valued at 11 or 1 (soft/hard hands)
+    - If a hand has an ace would bust with an 11, make it a 1
+
 
 MODERATE 
- - Add Betting
+ - Add Betting IN PROGRESS
  - Add Double Down/Split/Etc(?)
  - Make second card of dealer hidden when dealt
- - Add a play again option
- - Allow option to make a Ace valued at 11 or 1
+ - Add a play again option DONE
+ - Need to make a check to see if player automatically wins with a blackjack, they do not get option to hit/dd/stand etc.
 
 MINOR
  - Fix the vocabulary (Stand/Hit) DONE
  - Add line spaces BETTER
  - Clean up final if/else win prompt DONE(?)
  - Improve readability BETTER
+
+"After the boxes have finished playing, the dealer's hand is resolved by drawing cards until the hand achieves a total of 17 or higher. If the dealer has a total of 17 including an ace valued as 11 (a "soft 17"), some games require the dealer to stand while other games require another draw. The dealer never doubles, splits, or surrenders. If the dealer busts, all remaining player hands win. If the dealer does not bust, each remaining bet wins if its hand is higher than the dealer's and loses if it is lower."
--- Miscellaneous/blackjack/blackjack.py
+++ Miscellaneous/blackjack/blackjack.py
@@ -15,44 +15,67 @@ def card_value(card):
     else:
         return int(card[0])
 
+def adjust_score_for_ace(cards, score):
+    number_of_aces = sum(card[0] == 'Ace' for card in cards)
+    while score > 21 and number_of_aces > 0:
+        score -= 10  # Subtracting 10 as if changing an Ace from 11 to 1
+        number_of_aces -= 1
+    return score
+
 player_balance = 1000
 
 while True:
     player_wins = False
+    #double_down = False
+    tie = False
     deck = create_deck()
     random.shuffle(deck)
     player_card = [deck.pop(), deck.pop()]
     dealer_card = [deck.pop(), deck.pop()]
     
     player_score = sum(card_value(card) for card in player_card)
+    player_score = adjust_score_for_ace(player_card, player_score)
     dealer_score = sum(card_value(card) for card in dealer_card)
+    dealer_score = adjust_score_for_ace(dealer_card, dealer_score)
 
-    bet = int(input(f"Your balance is ${player_balance}. How much do you want to bet? ")) #need to add input validation so game doesnt break on non-int
+    bet = int(input(f"Your balance is ${player_balance}. How much do you want to bet? \n")) #need to add input validation so game doesnt break on non-int
     if bet > player_balance:
         print("You cannot bet more than your current balance.")
         continue
 
+    print("Cards Dealer Has:", dealer_card)
+    print("Score Of The Dealer:", dealer_score)
     while player_score <= 21:
+
         print("Cards Player Has:", player_card)
         print("Score Of The Player:", player_score)
         print("\n")
 
-        choice = input('What do you want? ["hit" to request another card, "stand" to stop]: ').lower()
-        if choice == "hit":
+        choice = input('What do you want? ["hit" to request another card, "dd" to double-down, or "stand" to stop]: ').lower()
+        if (choice == "hit" or choice == "dd"): #You can only double down on the first hand. Need to make a check so you cant double down after the first hand.
             new_card = deck.pop()
             player_card.append(new_card)
             player_score += card_value(new_card)
+            player_score = adjust_score_for_ace(player_card, player_score)
+            if choice == "dd":
+                bet *= 2
+                if bet > player_balance:
+                    print("You do not have enough balance to double down.")
+                    bet /= 2
+                else:
+                    print("Your bet is now ${}.".format(bet))
+                    #double_down = True
+                    break
         elif choice == "stand":
             break
         else:
             print("Invalid choice. Please try again.")
 
-    
-
-    while dealer_score <= 17:
+    while dealer_score < 17:
         new_card = deck.pop()
         dealer_card.append(new_card)
         dealer_score += card_value(new_card)
+        dealer_score = adjust_score_for_ace(dealer_card, dealer_score)
 
     print("\nCards Dealer Has:", dealer_card)
     print("Score Of The Dealer:", dealer_score)
@@ -60,30 +83,32 @@ while True:
     print("Score Of The Player:", player_score,"\n")
 
     #Determine the winner of the game
-    if (player_score == 21 and dealer_score != 21):
+    if (player_score == 21 and dealer_score != 21): #Win (BlackJack)
         print("BLACKJACK! Player wins!")
         player_wins = True
-    elif player_score > 21:
+    elif player_score > 21: #Player Bust
         print("BUST! Player loses!")
-    elif dealer_score > 21:
+    elif dealer_score > 21: #Dealer Bust
         print("DEALER BUST! Player wins!!!")
         player_wins = True
-    elif player_score > dealer_score:
-        print("Player wins (Player Has High Score than Dealer)")
+    elif player_score > dealer_score: #Win
+        print("Player wins (Player Has Higher Score than Dealer)")
         player_wins = True
-    elif dealer_score > player_score:
-        print("Dealer wins (Dealer Has High Score than Player)")
+    elif dealer_score > player_score: #Dealer Higher Score
+        print("Dealer wins (Dealer Has Higher Score than Player)")
     else:
+        tie = True
         print("It's a tie.")
         
 
     # After deciding the winner, update the player balance
-    if player_wins:
+    if (player_wins):
         winning_amount = int(1.5 * bet) #Calculate the amount won based on the bet and a 3:2 payout ratio.
-        player_balance += winning_amount #DOUBLE CHECK THIS
+        player_balance += winning_amount 
         print("You won! Your balance is now ${}.".format(player_balance))
         print("You won ${}.".format(winning_amount))
-
+    elif (tie):
+        print("Your balance is still ${}.".format(player_balance))
     else:
         player_balance -= bet
         print("You lost. Your balance is now ${}.".format(player_balance))
